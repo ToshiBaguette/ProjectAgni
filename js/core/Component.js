8480ee2a104aa7a5b68bafd2217e3f147ae0cca3
Grégory LORY <lory.gregory@live.fr>
2021-08-07 22:27:12 +0200

    Remove useless components + rewrite startup.js

--- js/core/Component.js
+++ js/core/Component.js
@@ -12,30 +12,4 @@ class Component {
 	}
 }
 
-class HealthComponent extends Component {
-	constructor() {
-		super();
-		this.hp = 100;
-		this.maxHp = 100;
-	}
-}
-
-class ManaComponent extends Component {
-	constructor() {
-		super();
-		this.mana = 10;
-		this.maxMana = 10;
-	}
-}
-
-class StatsComponent extends Component {
-	constructor() {
-		super();
-		this.strength = 1;
-		this.endurance = 1;
-		this.agility = 2;
-		this.chance = 3;
-	}
-}
-
-export { Component, HealthComponent, ManaComponent, StatsComponent };
+export { Component };
